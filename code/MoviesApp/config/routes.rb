58124cdd7c8e32c369cbc71bab8bb7c9839aa10e
Jeff Cohen <jeff@codeacademy.org>
2013-01-23 19:09:02 -0600

    Add link to add a new movie

--- code/MoviesApp/config/routes.rb
+++ code/MoviesApp/config/routes.rb
@@ -2,6 +2,9 @@ MoviesApp::Application.routes.draw do
 
   get '/movies', :controller => 'movies', :action => 'index', :as => :movies
 
+  get '/movies/new', :controller => 'movies', :action => 'new'
+
   get '/movies/:id', :controller => 'movies', :action => 'show', :as => :movie
+
 
 end
